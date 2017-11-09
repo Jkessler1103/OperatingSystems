a3e5bbbadbe64598bf42d7b326a2e8bad4675836
Kessler <jkessler510@studentubuntu14.ecc.iwcc.edu>
2017-11-09 16:15:43 -0600

    First bash scripts and variables.

---
+++ name.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="Jared Kessler"
+echo "Hello, $name"
+echo " "
+read -p "How old are you? " age
+echo " "
+read -p "What is your favorite color? " color
+echo "$name is $age years old, your favorite color is $color."
+echo " "
